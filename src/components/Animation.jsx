eaa57e1a36f91e84fda3f497dc69e56aa7687439
mag_daleneee <udokaineh04@gmail.com>
2024-02-26 13:22:52 +0100

    updated responsiveness

--- src/components/Animation.jsx
+++ src/components/Animation.jsx
@@ -1,20 +1,31 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react'
 import SlotCounter from 'react-slot-counter';
 
+
+
 function Animationn() {
+    const [showPlus, setShowPlus] = useState(false)
+
+    useEffect(() => {
+        const timer = setTimeout(() => {
+            setShowPlus(true)
+        }, 2500)
+        return () => clearInterval(timer)
+    }, [])
+
     return (
         <div className="slot-container animate__animated animate__fadeInUp">
             <div>
                 <div className="first-div">
-                    <SlotCounter value={18} animateUnchanged={true} autoAnimationStart={true}  duration={1.5}  />
+                    <SlotCounter value={18} animateUnchanged={true} autoAnimationStart={true} duration={1.5} />
                     <span className="months">Months</span>
                 </div>
                 <p className="second-para">Included Warranty</p>
             </div>
             <div>
                 <div className="first-div">
-                    <SlotCounter value={10000} animateUnchanged={true} autoAnimationStart={true}  duration={1.5} />
-                    <SlotCounter value={"+"}  duration={1.5} />
+                    <SlotCounter value={10000} animateUnchanged={true} autoAnimationStart={true} duration={1.5} />
+                    {showPlus && <span>+</span>}
                 </div>
                 <p className="second-para">Loyal customers</p>
             </div>
@@ -36,7 +47,7 @@ function Animationn() {
                             <img src="Assets/star-img.svg" alt="star icon" />
                         ]}
                         animateUnchanged={true}
-                        autoAnimationStart={true} 
+                        autoAnimationStart={true}
                         duration={1.5}
                     />
                 </div>
